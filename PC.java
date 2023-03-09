31addeb5975418bd88b955b3a59020fda74f4b13
GayathrySunil <122028291+GayathrySunil@users.noreply.github.com>
2023-03-09 15:23:04 +0530

    Add files via upload

---
+++ PC.java
@@ -0,0 +1,59 @@
+class item
+{
+int i ;
+boolean produced = false;
+synchronized void Produce( int x)
+	{
+	if(produced)
+	try
+	{wait();} catch (InterruptedException e){System.out.println(e);}
+i=x; // making
+System.out.println("Produced " +i);
+produced =true;
+notify();
+}
+
+
+ synchronized int Consume()
+{
+if(!produced)
+try
+	{wait();} catch (InterruptedException e){System.out.println(e);}
+	System.out.println("Consumed " +i);
+produced =false;
+notify();
+return i;
+}
+}
+
+
+class Producer extends Thread
+{
+	item q;
+	Producer(item qq){q=qq;}
+	public void run()
+	{
+		int i =0;
+		while(i!=10) q.Produce(i++);
+	}
+}
+
+class Consumer extends Thread
+{
+	item q;
+	Consumer(item qq){q=qq;}
+	public void run()
+	{
+			while(true) q.Consume();
+	}
+}
+
+class PC
+{
+public static void main(String args[])
+{
+	item i = new item();
+	Producer p = new Producer(i); p.start();
+	Consumer c = new Consumer(i); c.start();
+}
+}
